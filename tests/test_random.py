075e069f019516c2acefc7d6721cd6f508b4ac19
eunwoo song <presentsong@naver.com>
2026-09-07 03:05:03 +0900

    test: import public random helper

--- tests/test_random.py
+++ tests/test_random.py
@@ -1,4 +1,6 @@
 import pytest
+from py_simple_package.src.py_simple import pick_random_items as public_pick_random_items
+
 from py_simple_package.src.py_simple.easy_random import (
     roll_dice,
     flip_coin,
